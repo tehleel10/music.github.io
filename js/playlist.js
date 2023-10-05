ed85c97c1019b43a8a21dcdd13b000ecb761907f
vishalsatpute20 <136878175+vishalsatpute20@users.noreply.github.com>
2023-10-05 20:32:33 +0530

    update

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+      name: "Lover",
+      artist: "Taylor Swift",
+      img: "img1",
+      audio: "music1"
+    },
+    {
+      name: "Enchanted",
+      artist: "Taylor Swift",
+      img: "img2",
+      audio: "music2"
+    },
+    {
+      name: "Anti-Hero",
+      artist: "Taylor Swift",
+      img: "img3",
+      audio: "music3"
+    },
+    {
+      name: "Cruel Summer",
+      artist: "Taylor Swift",
+      img: "img4",
+      audio: "music4"
+    }
+  ]
